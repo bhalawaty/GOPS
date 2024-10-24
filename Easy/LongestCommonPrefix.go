1e1cbf4afa18aa568b081138f224a9edf63df0b9
bhalawaty <bilal.halawaty@gmail.com>
2024-10-24 16:44:19 +0300

    add problem link

--- Easy/LongestCommonPrefix.go
+++ Easy/LongestCommonPrefix.go
@@ -5,12 +5,20 @@ import (
 	"sort"
 )
 
+/*
+*
+https://leetcode.com/problems/longest-common-prefix/description/
+*/
 func main() {
 	arr := []string{"flower", "flow", "flight"}
 	result := longestCommonPrefix(arr)
 	fmt.Println(result)
 }
 
+/*
+*
+the syntax [:i] is used to slice a sequence up to, but not including, the index i.
+*/
 func longestCommonPrefix(strs []string) string {
 	if len(strs) == 1 { // handle only 1 element
 		return strs[0]
