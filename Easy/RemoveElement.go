90c268e7597b8420aa3fa78dc1de63598e9719df
bhalawaty <bilal.halawaty@gmail.com>
2024-10-27 19:22:55 +0300

    solve Remove Element problem

---
+++ Easy/RemoveElement.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/*
+https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+*/
+func main() {
+
+	var val int = 3
+	nums := []int{1, 2, 3, 3, 2}
+
+	fmt.Println(removeElement(nums, val))
+
+}
+
+func removeElement(nums []int, val int) int {
+	k := 0
+	for i, value := range nums {
+		if value != val {
+			if k != i { // Only assign if the current index i is different from k
+				nums[k] = value
+			}
+			k += 1
+		}
+	}
+	return k
+}
